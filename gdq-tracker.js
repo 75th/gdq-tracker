03cca4eae5bc52b82879d5e7e604d6c13ff5c1f8
Lanny Heidbreder <lanny@75thtrombone.com>
2017-01-13 14:04:50 -0600

    Documentation

--- gdq-tracker.js
+++ gdq-tracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Games Done Quick schedule tracker
 // @namespace    https://75thtrombone.com/
-// @version      0.1
+// @version      0.1.1
 // @description  Show current Games Done Quick event
 // @author       You
 // @match        https://gamesdonequick.com/schedule
